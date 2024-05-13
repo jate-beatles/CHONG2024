7be084ad861e3ea40d897f19383f9d258ed77239
jate-beatles <jate@live.cn>
2024-05-13 23:57:17 +0200

    canimotD5

---
+++ P-Algorithms_searching_sorting_indexing/C1-W2-Course&Code.py
@@ -0,0 +1,35 @@
+
+#########################################################################################################
+#########################################################################################################
+# Data Strucuture: The Dynamic Array 
+#
+#   > insert data
+#   > Delete data
+#   > Modify data
+#
+
+#### Dynamic Arrays
+# append the element to the end, space to grow at the end, the W.C. should as theta n of the n list
+#
+#### Dictionary Data Structure 
+#### Hash table - quickly to lookup a value of certain key
+#
+##################################################################################################
+##### Heaps, Min / Max - Heaps 
+#Abstract Data Type ---  adt
+# - Min Heap  & Max Heap 
+# A: [ 3, 4, 5, 7, 6, 18, 14, 9] 
+#>>>>>>>>>for the element i, the left child is 2i & 2i + 1 is the right child
+#>>>>>>>>>for i is the parent of 2i & (2i +1)
+#    A[i] has no parent which is the root 
+#    Heap is a special binary tree laid out on array
+#
+###### Min heap property - 
+#      > Every A[i] <= A[2i] 
+#      >       A[i] <= A[2i+1]
+#     1.> Every parent is less than and equal to its children 
+#
+
+#
+#
+#
