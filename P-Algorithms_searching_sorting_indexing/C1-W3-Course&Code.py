b1452c9862c2491e683ecf7072243c8537190e84
jate-beatles <jate@live.cn>
2024-05-23 23:47:43 +0200

    DominoD7

---
+++ P-Algorithms_searching_sorting_indexing/C1-W3-Course&Code.py
@@ -0,0 +1,50 @@
+#########################################################################################
+#### Aveage Case Complexity  & Recurrance Relations &  Randomization in ALG  
+####
+#
+#### Running Time of Algorithms: 
+
+def process_array(a): 
+    n = len(a)
+    result = 0.0 
+    for i in range(0,n):
+        for j in range(i+1,n):
+            if a[i] - a[j] ==3:
+                return 1
+            else: 
+                result = result + (a[i]-a[j])
+    return result
+
+#### Alg ust randomness 
+
+def geometric(p):
+    count = 0
+    if flip(p):  ### p is the number between 0-1, return the true with prob. p and false
+        return 1 + geometric(p)
+    else:
+        return 1
+####        T(P) = 1(1-P) + (1+T(P)) * P
+
+## W.C. is infinity 
+
+#  why randomness
+#       input ----- alg ----- output ### avg randomness the avg is less than w.c. 
+
+
+#### Why random in data structure 
+# 
+#### Recurrences 
+
+# def alg ( input_array):
+#   if  len(input_array) <= m: 
+#       alg(new_arrray_1) 
+#       .....
+#       alg(new_array_2) 
+
+#>>>>> T(n) = aT(n/b) + theta( n)   # if n>m 
+
+
+
+
+
+
